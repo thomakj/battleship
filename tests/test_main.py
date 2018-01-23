675786ea6c599cdc3b85eb8b6b43db5c655ee7b0
Thomas Kjøglum <>
2018-01-23 18:50:49 +0100

    Testing two players

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,5 +10,11 @@ class TestPlayer(unittest.TestCase):
         player_name = self.player.get_name()
         self.assertEqual("Tester Testersen", player_name)
 
+    def test_two_players_name(self):
+        self.player1 = Player("Player1")
+        self.player2 = Player("Player2")
+        self.assertEqual("Player1", self.player1.get_name())
+        self.assertEqual("Player2", self.player2.get_name())
+
 if __name__ == '__main__':
     unittest.main()
