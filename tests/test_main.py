9915664b324813d99e600e3ac5641943a6676901
Thomas Kjøglum <>
2018-01-23 18:27:40 +0100

    Testing the player name

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+import unittest
+from app.player import Player
+
+class TestPlayer(unittest.TestCase):
+
+    """docstring for TestPlayer."""
+
+    def test_player_name_on_object_creation(self):
+        self.player = Player("Tester Testersen")
+        player_name = self.get_name()
+        self.assertEqual("Tester Testersen", player_name)
+
+if __name__ == '__main__':
+    unittest.main()
